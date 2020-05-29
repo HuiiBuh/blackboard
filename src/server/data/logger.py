62f2ba4e10ac749531698044f78c77fac9a4bfc4
Devel 484 <p.kelbing96@web.de>
2020-05-29 13:22:10 +0200

    Add streamhandler(formatting) #20

--- src/server/data/logger.py
+++ src/server/data/logger.py
@@ -1,9 +1,16 @@
 import logging
-from logging import FileHandler
+from logging import FileHandler, Formatter, StreamHandler
 
 Logger = logging.getLogger("Blackboard")
 Logger.setLevel(logging.DEBUG)
 
 fh = FileHandler("api.log")
+sh = StreamHandler()
 
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+sh.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+Logger.addHandler(sh)
 Logger.addHandler(fh)
